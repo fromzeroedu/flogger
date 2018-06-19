89e1f51e28d1af0160753c850236459d79888d3a
Jorge Escobar <jorge@fromzero.io>
2018-06-19 19:34:44 -0400

    Removing live flag from author

--- author/models.py
+++ author/models.py
@@ -5,13 +5,11 @@ class Author(db.Model):
     full_name = db.Column(db.String(80))
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(128))
-    live = db.Column(db.Boolean)
 
-    def __init__(self, full_name, email, password, live=True):
+    def __init__(self, full_name, email, password):
         self.full_name = full_name
         self.email = email
         self.password = password
-        self.live = live
 
     def __repr__(self):
         return '<Author %r>' % self.full_name
