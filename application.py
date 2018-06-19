15ac7481a445d4ba804b6eb12134118ec180fb43
Jorge Escobar <jorge@fromzero.io>
2018-06-18 20:01:45 -0400

    step-0

--- application.py
+++ application.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flaskext.markdown import Markdown
 
 # setup db
 db = SQLAlchemy()
@@ -19,15 +18,10 @@ def create_app(**config_overrides):
     db.init_app(app)
     migrate = Migrate(app, db)
 
-    # Markdown
-    Markdown(app)
-
     # import blueprints
-    from author.views import author_app
     from blog.views import blog_app
 
     # register blueprints
-    app.register_blueprint(author_app)
     app.register_blueprint(blog_app)
 
     return app
