67c379d43f07dddf6e21366899513d319e63c57f
Jorge Escobar <jorge@fromzero.io>
2018-09-10 19:26:29 -0400

    Starting step-4

--- author/views.py
+++ author/views.py
@@ -1,9 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 from author.models import Author
+from author.forms import RegisterForm
 
 author_app = Blueprint('author_app', __name__)
 
 @author_app.route('/register', methods=('GET', 'POST'))
 def register():
-    return 'Author Registration'
+    form = RegisterForm()
+    return render_template('author/register.html', form=form)
