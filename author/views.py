613873624744ee4df2233bfdeae38fbc46e87b66
Jorge Escobar <jorge@fromzero.io>
2018-10-03 19:31:51 -0400

    Completed step-5

--- author/views.py
+++ author/views.py
@@ -8,4 +8,6 @@ author_app = Blueprint('author_app', __name__)
 @author_app.route('/register', methods=('GET', 'POST'))
 def register():
     form = RegisterForm()
+    if form.validate_on_submit():
+        return "Validated"
     return render_template('author/register.html', form=form)
