1aa5d89eab706621e74585da3dc5094fa15b2b62
Jorge Escobar <jorge@fromzero.io>
2018-10-05 18:28:37 -0400

    started step-11

--- author/forms.py
+++ author/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import validators, StringField, PasswordField, ValidationError
 from wtforms.fields.html5 import EmailField
+from werkzeug.security import check_password_hash
 
 from author.models import Author
 
@@ -10,6 +11,23 @@ class LoginForm(FlaskForm):
             validators.Required(),
             validators.Length(min=4, max=80)
         ])
+
+    def validate(self):
+        rv = FlaskForm.validate(self)
+        if not rv:
+            return False
+
+        author = Author.query.filter_by(
+            email=self.email.data,
+            ).first()
+
+        if author:
+            if not check_password_hash(author.password, self.password.data):
+                self.password.errors.append('Incorrect email or password')
+                return False
+            return True
+        else:
+            self.password.errors.append('Incorrect email or password')
 
 class RegisterForm(FlaskForm):
     full_name = StringField('Full Name', [validators.InputRequired()])
