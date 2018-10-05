07a31285a354e4b56c337d558e7a0fcd27200eec
Jorge Escobar <jorge@fromzero.io>
2018-10-05 19:09:49 -0400

    finished step-12

--- author/views.py
+++ author/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, session, url_for
 from werkzeug.security import generate_password_hash
 
 from application import db
