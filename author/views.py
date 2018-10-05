bb2fe7591ed534c389dc18204b805e5ecbb5f0f8
Jorge Escobar <jorge@fromzero.io>
2018-10-04 20:07:12 -0400

    started step-8

--- author/views.py
+++ author/views.py
@@ -9,5 +9,13 @@ author_app = Blueprint('author_app', __name__)
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data)
+        author = Author(
+            form.full_name.data,
+            form.email.data,
+            hashed_password
+        )
+        db.session.add(author)
+        db.session.commit()
         return "Validated"
     return render_template('author/register.html', form=form)
