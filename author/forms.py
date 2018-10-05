6680d0095c5c758cc58a867f4defc62620af7b0a
Jorge Escobar <jorge@fromzero.io>
2018-10-05 14:27:09 -0400

    finished step-9

--- author/forms.py
+++ author/forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import validators, StringField, PasswordField
 from wtforms.fields.html5 import EmailField
 
+from author.models import Author
+
 class RegisterForm(FlaskForm):
     full_name = StringField('Full Name', [validators.InputRequired()])
     email = EmailField('Email address', [validators.InputRequired(), validators.Email()])
@@ -12,3 +14,8 @@ class RegisterForm(FlaskForm):
     confirm = PasswordField('Repeat Password', [
             validators.EqualTo('password', message='Passwords must match'),
     ])
+
+    def validate_email(self, email):
+        author = Author.query.filter_by(email=email.data).first()
+        if author is not None:
+            raise ValidationError('Email already in use, please use a different one.')
