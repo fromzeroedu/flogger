f273578dc8429abff6c2ae22532a8af04cc226da
Jorge Escobar <jorge@fromzero.io>
2018-10-06 15:26:49 -0400

    started step-12

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, session
 
 blog_app = Blueprint('blog_app', __name__)
 
 @blog_app.route('/')
 def index():
+    if session.get('full_name'):
+        return f'Hi { session["full_name"] }'
     return 'Blog Home'
