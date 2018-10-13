0053b91f947707381ebf83b88d295c3fcdc97676
Jorge Escobar <jorge@fromzero.io>
2018-10-12 20:38:36 -0400

    starting step-19

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,4 @@ blog_app = Blueprint('blog_app', __name__)
 
 @blog_app.route('/')
 def index():
-    if session.get('full_name'):
-        return f'Hi { session["full_name"] }'
-    return 'Blog Home'
+    return render_template('blog/index.html')
