6354a8b1a0ac53f0b0f0fe8487c90860e270fd72
Jorge Escobar <jorge@fromzero.io>
2018-10-14 13:30:17 -0400

    starting step-21

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, session, render_template, session
+from slugify import slugify
 
 from blog.models import Post, Category
 from blog.forms import PostForm
@@ -13,6 +14,33 @@ def index():
 @blog_app.route('/post', methods=('GET', 'POST'))
 def post():
     form = PostForm()
+
+    if form.validate_on_submit():
+        if form.new_category.data:
+            new_category = Category(form.new_category.data)
+            db.session.add(new_category)
+            db.session.flush()
+            category = new_category
+        else:
+            category = form.category.data
+
+        author = Author.query.get(session['id'])
+        title = form.title.data.strip()
+        body = form.body.data.strip()
+        post = Post(
+            author=author,
+            title=title,
+            body=body,
+            category=category,
+        )
+
+        db.session.add(post)
+        db.session.commit()
+
+        # do the slug
+        slug = slugify(str(post.id) + '-' + post.title)
+        post.slug = slug
+        db.session.commit()
 
     return render_template('blog/post.html',
         form=form
