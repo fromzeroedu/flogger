0a85a17bfee2544fe45154e636d48309ab2f80e5
Jorge Escobar <jorge@fromzero.io>
2018-10-14 13:24:35 -0400

    finished step-20

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,19 @@
-from flask import Blueprint, session, render_template
+from flask import Blueprint, session, render_template, session
+
+from blog.models import Post, Category
+from blog.forms import PostForm
+from author.models import Author
 
 blog_app = Blueprint('blog_app', __name__)
 
 @blog_app.route('/')
 def index():
     return render_template('blog/index.html')
+
+@blog_app.route('/post', methods=('GET', 'POST'))
+def post():
+    form = PostForm()
+
+    return render_template('blog/post.html',
+        form=form
+    )
