71502a13dbf7b1e796ff60b4ff91499207a21037
Jorge Escobar <jorge@fromzero.io>
2018-10-22 13:51:07 -0400

    starting step-28

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import validators, StringField, TextAreaField, SelectField
+from wtforms import validators, StringField, TextAreaField, SelectField, FileField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from flask_wtf.file import FileAllowed
 
 from blog.models import Category
 
@@ -8,6 +9,9 @@ def categories():
     return Category.query
 
 class PostForm(FlaskForm):
+    image = FileField('Image', validators=[
+        FileAllowed(['jpg', 'png'], 'We only accept JPG or PNG images')
+    ])   
     title = StringField('Title', [
             validators.InputRequired(),
             validators.Length(max=80)
